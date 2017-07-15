124e4fd01f286e6f16496737bc68d34b9d6d7246
ambujsharma23 <ambujsharma23@gmail.com>
2017-07-15 15:04:07 +0530

    always show

--- Scripts/et_custom.js
+++ Scripts/et_custom.js
@@ -1,37 +1,37 @@
 ﻿$(function () {
 
-    $('.appendTextBox').hide();
-    $('.hideit').hide();
-    $("#Q802").hide();
-    $('#Q804').hide();
-    $('#Q805').hide();
-    $('#Q806').hide();
-    $('#Q807').hide();
-    $('#Q808').hide();
-    $('#Q809').hide();
-    $('#Q810').hide();
-    $('#Q811').hide();
-    $('#Q812').hide();
-    $('#Q813').hide();
-    $('#Q814').hide();
-    $('#Q815').hide();
-    $('#Q816').hide();
-    $('#Q817').hide();
-    $('.H802').hide();
-    $('.H804').hide();
-    $('.H805').hide();
-    $('.H806').hide();
-    $('.H807').hide();
-    $('.H808').hide();
-    $('.H809').hide();
-    $('.H810').hide();
-    $('.H811').hide();
-    $('.H812').hide();
-    $('.H813').hide();
-    $('.H814').hide();
-    $('.H815').hide();
-    $('.H816').hide();
-    $('.H817').hide();
+    //$('.appendTextBox').hide();
+    //$('.hideit').hide();
+    //$("#Q802").hide();
+    //$('#Q804').hide();
+    //$('#Q805').hide();
+    //$('#Q806').hide();
+    //$('#Q807').hide();
+    //$('#Q808').hide();
+    //$('#Q809').hide();
+    //$('#Q810').hide();
+    //$('#Q811').hide();
+    //$('#Q812').hide();
+    //$('#Q813').hide();
+    //$('#Q814').hide();
+    //$('#Q815').hide();
+    //$('#Q816').hide();
+    //$('#Q817').hide();
+    //$('.H802').hide();
+    //$('.H804').hide();
+    //$('.H805').hide();
+    //$('.H806').hide();
+    //$('.H807').hide();
+    //$('.H808').hide();
+    //$('.H809').hide();
+    //$('.H810').hide();
+    //$('.H811').hide();
+    //$('.H812').hide();
+    //$('.H813').hide();
+    //$('.H814').hide();
+    //$('.H815').hide();
+    //$('.H816').hide();
+    //$('.H817').hide();
 
     $("#Q801_D1").trigger("change");
     $("#Q808_D1").trigger("change");
@@ -117,7 +117,7 @@
         $selectedValue = $('#Q808_D1').val();
 
         if ($selectedValue != 0 && $selectedValue != 1) {
-            $('#Q809').show(); // NO Header Hide
+            $('#Q809').show(); // NO Header Hide+
         }
         else {
             $('#Q809').hide();
